eeba68b178f004b0dae896265893dab59063b037
Prince Maurya <princemaurya002@gmail.com>
2025-05-27 14:10:20 +0530

    Additional Designs Completed

--- app/src/main/java/com/princemaurya/harmonicaura/ProgressTrackerFragment.kt
+++ app/src/main/java/com/princemaurya/harmonicaura/ProgressTrackerFragment.kt
@@ -5,8 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
-import androidx.cardview.widget.CardView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -15,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
+ * Use the [ProgressTrackerFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeFragment : Fragment() {
+class ProgressTrackerFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -36,17 +34,7 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        
-        // Find the chakra card view
-        view.findViewById<CardView>(R.id.chakra_card)?.setOnClickListener {
-            // Navigate to ChakraFragment
-            findNavController().navigate(R.id.action_homeFragment_to_chakraFragment)
-        }
+        return inflater.inflate(R.layout.fragment_progress_tracker, container, false)
     }
 
     companion object {
@@ -56,12 +44,12 @@ class HomeFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
+         * @return A new instance of fragment ProgressTrackerFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
+            ProgressTrackerFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
